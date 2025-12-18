f1415d63d52df10d861504ec0ea768ec483b4c02
Marco Galeri <marco.galeri@studio.unibo.it>
2025-12-18 15:59:28 +0100

    chore: Update gitignore

---
+++ src/main/scala/wikipediaAnalytics.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.rdd.RDD
+import scala.collection.mutable
+
+object WikipediaAnalytics {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("Wikipedia Bus Factor")
+    val sc   = new SparkContext(conf)
+
+    val allData    = sc.textFile("dataset/wikimedia_dumps/2025-11.enwiki.*.tsv.bz2")
+    val parsedData = allData.map(_.split("\t"))
+    val data       = parsedData.take(100).toSeq
+    println("Sample Data:")
+    data.foreach(row => println(row.mkString("\t")))
+  }
+}
